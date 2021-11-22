6d08f52f209ef30d0e03e2f17b84a0a2bedd35e4
GH0STAV0 <you@example.com>
2021-11-21 19:20:28 -0800

    add new project

--- src/package.sh
+++ src/package.sh
@@ -4,7 +4,7 @@
 set -e
 #export NO_VNC_HOME=/usr/share/novnc
 whoami
-
+cp /root/install/geckodriver13 /usr/bin/
 mkdir -p /root/EXTRAT/
 echo "Install Package - on  based ubuntu 20.04 LTS"
 echo "Installing  dbus-x11 xauth xinit x11-xserver-utils xdg-utils"
@@ -41,7 +41,7 @@ echo "Install bash color"
 echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> /root/.bashrc
 #chmod +x /root/install_add/geckodriver && cp /root/install_add/geckodriver /usr/bin/geckodriver13
 echo "Install Package PIP-3 ***** pymysql pyvirtualdisplay faker-e164 Faker PySocks stem  bs4 selenium  ConfigParser lxml  speechrecognition requests pyvirtualdisplay pydub"
-#pip3 install selenium==4.0.0.a1
+pip3 install selenium==4.0.0.a1 emojis emoji mysql-connector
 #pip3 install pymysql pyvirtualdisplay faker-e164 Faker PySocks stem  bs4  ConfigParser lxml  speechrecognition requests \
  #pyvirtualdisplay pydub pyautogui emojis emoji mysql-connector
 
